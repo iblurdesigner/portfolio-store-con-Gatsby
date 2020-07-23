f25d732d0e860fed57c52bc60b2b9699a879b801
iblurdesigner <nevomen@gmail.com>
2020-07-23 17:39:50 -0500

    Router en Gatsby y componente Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Jumbo } from "../components"
+import "../components/layout.css"
 
 import { Layout, SEO } from "../components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Jumbo />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/gracias/">Go to gracias</Link>
+    <Link to="/cancelada/">Ir a compra cancelada</Link>
   </Layout>
 )
 
