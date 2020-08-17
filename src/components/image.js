0ce1218b9598cfbf73438ecbea24a892aca7ca0d
iblurdesigner <nevomen@gmail.com>
2020-08-17 18:30:48 -0500

    useStaticQuery

--- src/components/image.js
+++ src/components/image.js
@@ -1,25 +1,21 @@
 import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import { graphql, StaticQuery, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
 export default function Image() {
-  return (
-    <StaticQuery
-      query={graphql`
-        query GET_IMAGE {
-          icon: file(relativePath: { eq: "icon.png" }) {
-            childImageSharp {
-              fluid(maxWidth: 1000) {
-                ...GatsbyImageSharpFluid
-              }
+  const data = useStaticQuery(
+    graphql`
+      query GET_IMAGE {
+        icon: file(relativePath: { eq: "icon.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
             }
-            name
           }
+          name
         }
-      `}
-      render={data => (
-        <Img fluid={data[data.icon.name].childImageSharp.fluid} />
-      )}
-    />
+      }
+    `
   )
+  return <Img fluid={data[data.icon.name].childImageSharp.fluid} />
 }
